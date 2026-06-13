9b556e7ee9e47587ebc70d4ed2bd4bcbd45b746a
Arroz <pedroalckmin1503@gmail.com>
2026-06-13 10:49:03 -0300

    Add files via upload

---
+++ 1JOD Fibonacci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n1 = 1;
+        int n2 = 0;
+        int CD;
+
+        Console.WriteLine("Qual a quantidade da sequencia que voce deseja : ");
+        CD = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("------------------------------------------------");
+        for (int i = 0; i < CD; i++)
+        {
+            int prox = n1 + n2;
+            Console.WriteLine(prox);
+
+            n1 = n2;
+            n2 = prox;
+        }
+    }
+}
