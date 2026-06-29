755185fa90e806513fa9dcae48d49795a4bdfd16
Arroz <pedroalckmin1503@gmail.com>
2026-06-29 11:02:26 -0300

    Add files via upload

---
+++ 1JodPA.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random gerador = new Random();
+        int numeroRandom = gerador.Next(100);
+
+        int n2;
+
+        while (true)
+        {
+            Console.Write("Qual o número que a máquina sorteou: ");
+
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            if (n2 == numeroRandom)
+            {
+                Console.WriteLine("Você acertou!");
+                break;
+            }
+            else if (n2 > numeroRandom)
+            {
+                Console.WriteLine("O número sorteado é menor.");
+            }
+            else
+            {
+                Console.WriteLine("O número sorteado é maior.");
+            }
+        }
+    }
+}
